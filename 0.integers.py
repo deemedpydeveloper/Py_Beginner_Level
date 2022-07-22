404afc197921d680db811d9a5aedebc27171317c
tejendra <tejendrayerra2000.4@gmail.com>
2022-07-22 17:52:34 +0530

    Add files via upload

---
+++ 0.integers.py
@@ -0,0 +1,5 @@
+# Integers.
+
+# a number with no quotes around is treated as an integer.
+# all whole numbers whether positive or negative come under integers in programming lingo.
+print(123 + 456)  
