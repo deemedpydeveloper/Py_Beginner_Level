f618919e74b04b7016f279bb17b844a1a7d38f30
tejendra <tejendrayerra2000.4@gmail.com>
2022-07-23 19:30:33 +0530

    Delete 1.Band_Name_Genearator_Project directory

--- 1.Band_Name_Genearator_Project/2.printing.py
+++
@@ -1,4 +0,0 @@
-print("Day 1 - Python Print Function")
-print("THe function is declared like this:")
-print("print('what to print')")  # single quotes is used to avoid the computer to take 'print' as a function.
-# We can use single quotes or double quotes for printing out in console.
--- 1.Band_Name_Genearator_Project/9.switching_variable_values.py
+++
@@ -1,9 +0,0 @@
-# Switch the variable values.
-
-a = input("a: ")
-b = input("b: ")
-c = a  # creating a variable to alter the values of a and b.
-a = b
-b = c
-print("a = " + a)
-print("b = " + b)
