ca1f82098dc026a190e992f6e29de85602f5aecf
tejendra <tejendrayerra2000.4@gmail.com>
2022-07-23 19:33:32 +0530

    Add files via upload

---
+++ 3.Treasure_Island/0.comparision_operators.py
@@ -0,0 +1,8 @@
+# Comparision Operators.
+
+## > ----> greater than.
+## < ----> less than.
+## >= ---> greater than or equals to.
+## <= ---> less than or equals to.
+## == ---> equals to.
+## != ---> not equal to.
